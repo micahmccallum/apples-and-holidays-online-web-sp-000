172123fe51175e8e538b50ea2ce1cb6f25675d2e
Micah McCallum <micahmccallum@outlook.com>
2020-03-29 19:29:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,8 +50,8 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each do |name, supply_array|
-      holiday = (name.to_s.split("_").collect { |word| word.capitalize }).join(" ")
+    holiday.each do |holiday_name, supply_array|
+      holiday = (holiday_name.to_s.split("_").collect { |word| word.capitalize }).join(" ")
       supplies = supply_array.join(", ")
       puts "  #{holiday}: #{supplies}"
     end
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday_name
 
 end
